607b2ee5f4087541a981d2a7199da2c324941b50
Sachin Patil <56148328+sachin1228@users.noreply.github.com>
2026-08-04 01:51:49 +0530

    Update ThreadCard.tsx

--- apps/web/components/communities/threads/ThreadCard.tsx
+++ apps/web/components/communities/threads/ThreadCard.tsx
@@ -520,7 +520,7 @@ export function ThreadCard({
         </div>
       ) : (
         <article className={`group ${isLast ? "" : "border-b border-border"}`}>
-          <Link href={threadHref} className="block py-8 px-8">
+          <Link href={threadHref} className="block">
             {innerContent}
           </Link>
         </article>
